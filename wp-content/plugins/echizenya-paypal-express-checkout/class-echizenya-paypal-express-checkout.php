6d126dacecda78bd031e58596bf0d50111848360
echizenyayota <virtualmisso30@gmail.com>
2018-09-03 15:21:34 +0900

    プラグインの設定画面を表示する

--- wp-content/plugins/echizenya-paypal-express-checkout/class-echizenya-paypal-express-checkout.php
+++ wp-content/plugins/echizenya-paypal-express-checkout/class-echizenya-paypal-express-checkout.php
@@ -8,4 +8,30 @@ class Echizenya_PayPal_Express_Checkout {
   public function __construct() {
     add_action( 'admin_menu', array($this, 'paypalexpresscheckout_add_admin_menu') );
   }
+
+  // 管理画面にサブメニューを表示するメソッド
+  public function paypalexpresscheckout_add_admin_menu() {
+      add_options_page(
+        'Settings Admin',
+        'Echizenya PayPal ExpressCheckout',
+        'manage_options',
+        'paypal-settings-group',
+        array( $this, 'create_admin_page' )
+    );
+  }
+
+  // 設定画面を表示するメソッド
+  public function create_admin_page() {
+    $this->options = get_option( 'echizenya_paypal_express_checkout' );
+    ?>
+    <div class="wrap">
+       <h2>Echizenya PayPal ExpressCheckout Settings</h2>
+       <form method="post" action="options.php">
+         <?php settings_fields( 'paypal-settings-group' ); ?>
+         <?php do_settings_sections( 'paypal-settings-group' ); ?>
+         <?php submit_button(); ?>
+       </form>
+    </div>
+    <?php
+  }
 }
