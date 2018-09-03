fc15b91cbd103aa3359001c5253cf2309aca54e6
echizenyayota <virtualmisso30@gmail.com>
2018-09-03 15:00:47 +0900

    プラグインに必要なディレクトリ・ファイルの作成とプラグインヘッダの書き方

---
+++ wp-content/plugins/echizenya-paypal-express-checkout/echizenya-paypal-express-checkout.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Echizenya PayPal Express Checkout
+Plugin URI: https://example.com
+Description: Blog Commerce PayPal Express Checkout
+Version: 0.0.0
+Author: echizenya
+Author URI: https://e-yota.com
+License: GPLv2 or later
+Text Domain: echizenya-paypal-express-checkout
+*/
